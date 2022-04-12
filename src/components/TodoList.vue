1cb93b9686fc72acc3ea75a5547c485830db6abd
desain <1810479856@qq.com>
2022-04-12 19:34:38 +0800

    初始

---
+++ src/components/TodoList.vue
@@ -0,0 +1,43 @@
+<template>
+    <ul class="todo-main">
+        <TodoItem 
+          v-for="todoObj in todos"
+          :key="todoObj.id"
+          :todo="todoObj"
+          :deleteItem="deleteItem"
+          :changeChecked="changeChecked"
+        />
+
+    </ul>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+    name: 'TodoList',
+    components:{
+        TodoItem
+    },
+    props:['todos', 'deleteItem', 'changeChecked']
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
